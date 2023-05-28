016c41037d1d2da80c7e8d1c3eb815261573c518
Bartosz Womuczył <bartosz.womuczyl@onet.pl>
2023-05-28 14:04:13 +0200

    First test implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -1,7 +1,11 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UltimateQATest extends PageSetup {
 
@@ -13,6 +17,14 @@ public class UltimateQATest extends PageSetup {
 
     @Test
     public void testOne() {
+
+        WebElement buttonUsingId = driver.findElementById("idExample");
+        buttonUsingId.click();
+
+        WebElement buttonSuccessText = driver.findElementByClassName("entry-title");
+        assertEquals("Button success", buttonSuccessText.getText());
+    }
+
 
 
 
